f760314d858ab00b29058255725300a89f84ad74
Uzytkownik <uzytkownik@kurs.GIT.plgit config --global user.email uzytkownik@kurs.GIT.pl>
2022-02-28 13:08:18 +0100

    Validation Class + Validation Tests

--- src/main/java/Validation.java
+++ src/main/java/Validation.java
@@ -1,7 +1,16 @@
 public class Validation {
 
+
+    public boolean validateTheCoordinates(String country) {
+        return country.matches("^[-+]?([1-8]?\\d(\\.\\d+)?|90(\\.0+)?),\\s*[-+]?(180(\\.0+)?|((1[0-7]\\d)|([1-9]?\\d))(\\.\\d+)?)$");
+    }
+
     public boolean validateIfCityNameIsEmpty(String city) {
-        return city.length() == 0;
+        return city == null || city.length() == 0;
+    }
+
+    public boolean validateIfCountryNameIsEmpty(String country) {
+        return country == null || country.length() == 0;
     }
 
 
