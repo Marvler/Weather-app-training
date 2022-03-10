59b953e1b4f0c9bcaec11ff9e133d2684eac41a9
Rafał Zieliński <96017423+rafalz94@users.noreply.github.com>
2022-03-10 13:52:38 +0100

    Lombok adaptation in Location and WeatherData

--- src/main/java/model/WeatherData.java
+++ src/main/java/model/WeatherData.java
@@ -1,5 +1,10 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class WeatherData {
 
     private final String cityName;
@@ -8,39 +13,6 @@ public class WeatherData {
     private final long humidity;
     private final String windDirection;
     private final double windSpeed;
-
-    public WeatherData(String cityName, double temperature, long pressure, long humidity, String windDirection, double windSpeed) {
-        this.cityName = cityName;
-        this.temperature = temperature;
-        this.pressure = pressure;
-        this.humidity = humidity;
-        this.windDirection = windDirection;
-        this.windSpeed = windSpeed;
-    }
-
-    public String getCityName() {
-        return cityName;
-    }
-
-    public double getTemperature() {
-        return temperature;
-    }
-
-    public long getPressure() {
-        return pressure;
-    }
-
-    public long getHumidity() {
-        return humidity;
-    }
-
-    public String getWindDirection() {
-        return windDirection;
-    }
-
-    public double getWindSpeed() {
-        return windSpeed;
-    }
 
     @Override
     public String toString() {
